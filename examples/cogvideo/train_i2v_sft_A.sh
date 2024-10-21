43def08ba2a524948a9dad7fbc4695b862c87d9e
Wang-Xiaodong1899 <1443472206@qq.com>
2024-10-21 10:12:46 +0800

    minor

--- examples/cogvideo/train_i2v_sft_A.sh
+++ examples/cogvideo/train_i2v_sft_A.sh
@@ -15,11 +15,11 @@ accelerate launch --multi-gpu --config_file /workspace/wxd/diffusers/examples/co
   --tracker_name cogvideo-A4-clean-image-sft \
   --output_dir /volsparse2/wxd/ckpt/cogvideo/cogvideox-A4-clean-image-sft-bs1 \
   --height 480 --width 720 --fps 8 --max_num_frames 33 \
-  --train_batch_size 1 \
+  --train_batch_size 2 \
   --num_train_epochs 1000 \
-  --checkpointing_steps 2000 \
+  --checkpointing_steps 500 \
   --gradient_accumulation_steps 1 \
-  --learning_rate 1e-4 \
+  --learning_rate 5e-5 \
   --lr_scheduler cosine_with_restarts \
   --lr_warmup_steps 100 \
   --lr_num_cycles 1 \
